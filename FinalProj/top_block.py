de91821c357e04a7841205d387e25fec44112da4
Unknown <jeffchrisope@live.com>
2018-06-18 12:36:44 -0700

    More stuff

--- FinalProj/top_block.py
+++ FinalProj/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Sun Jun 17 13:11:29 2018
+# Generated: Mon Jun 18 12:34:34 2018
 ##################################################
 
 if __name__ == '__main__':
@@ -18,6 +18,7 @@ if __name__ == '__main__':
 
 from PyQt4 import Qt
 from gnuradio import blocks
+from gnuradio import channels
 from gnuradio import digital
 from gnuradio import eng_notation
 from gnuradio import fec
@@ -26,7 +27,6 @@ from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from grc_gnuradio import blks2 as grc_blks2
 from optparse import OptionParser
-import limesdr
 import sys
 
 
@@ -63,7 +63,7 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32e3
+        self.samp_rate = samp_rate = 50e3
         
         
         self.ccsds_encoder_var = ccsds_encoder_var = fec.ccsds_encoder_make(frame_sz_bytes * 8, 0, fec.CC_TAILBITING)
@@ -76,59 +76,72 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.limesdr_sink_0 = limesdr.sink(0,
-        		       2,
-        		       1,
-        		       0,
-        		       0,
-        		       '',
-        		       144e6,
-        		       32e3,
-        		       1,
-        		       0,
-        		       10e6,
-        		       0,
-        		       10e6,
-        		       1,
-        		       1,
-        		       1,
-        		       0,
-        		       10e6,
-        		       0,
-        		       10e6,
-        		       0,
-        		       0,
-        		       0,
-        		       0,
-        		       60,
-        		       60)
         self.fec_extended_encoder_0 = fec.extended_encoder(encoder_obj_list=ccsds_encoder_var, threading='capillary', puncpat='11')
+        self.fec_extended_decoder_0 = fec.extended_decoder(decoder_obj_list=cc_decoder_var, threading='capillary', ann=None, puncpat='11', integration_period=10000)
+        self.digital_map_bb_0 = digital.map_bb((-1,1))
         self.digital_dxpsk_mod_0 = digital.dbpsk_mod(
         	samples_per_symbol=2,
         	excess_bw=0.35,
         	mod_code="gray",
-        	verbose=True,
+        	verbose=False,
         	log=False)
         	
+        self.digital_dxpsk_demod_0 = digital.dbpsk_demod(
+        	samples_per_symbol=2,
+        	excess_bw=0.35,
+        	freq_bw=6.28/100.0,
+        	phase_bw=6.28/100.0,
+        	timing_bw=6.28/100.0,
+        	mod_code="gray",
+        	verbose=False,
+        	log=False
+        )
+        self.channels_channel_model_0 = channels.channel_model(
+        	noise_voltage=0.5,
+        	frequency_offset=0.0,
+        	epsilon=1.0,
+        	taps=(1.0 + 1.0j, ),
+        	noise_seed=0,
+        	block_tags=False
+        )
         self.blocks_unpack_k_bits_bb_0 = blocks.unpack_k_bits_bb(16)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
+        self.blocks_pack_k_bits_bb_0 = blocks.pack_k_bits_bb(16)
         self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/home/jeff/EE420/FinalProj/testdat.txt', True)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/home/jeff/EE420/Repo/EE420SDR/FinalProj/fecout.txt', False)
+        self.blocks_file_sink_0.set_unbuffered(True)
+        self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
         self.blks2_packet_encoder_1 = grc_blks2.packet_mod_b(grc_blks2.packet_encoder(
         		samples_per_symbol=2,
         		bits_per_symbol=1,
         		preamble='',
         		access_code='',
-        		pad_for_usrp=False,
+        		pad_for_usrp=True,
         	),
         	payload_length=0,
+        )
+        self.blks2_packet_decoder_1 = grc_blks2.packet_demod_b(grc_blks2.packet_decoder(
+        		access_code='',
+        		threshold=-1,
+        		callback=lambda ok, payload: self.blks2_packet_decoder_1.recv_pkt(ok, payload),
+        	),
         )
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.blks2_packet_decoder_1, 0), (self.digital_map_bb_0, 0))    
         self.connect((self.blks2_packet_encoder_1, 0), (self.digital_dxpsk_mod_0, 0))    
-        self.connect((self.blocks_file_source_0, 0), (self.blocks_unpack_k_bits_bb_0, 0))    
+        self.connect((self.blocks_char_to_float_0, 0), (self.fec_extended_decoder_0, 0))    
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))    
+        self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.blocks_file_sink_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_unpack_k_bits_bb_0, 0))    
         self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.fec_extended_encoder_0, 0))    
-        self.connect((self.digital_dxpsk_mod_0, 0), (self.limesdr_sink_0, 0))    
+        self.connect((self.channels_channel_model_0, 0), (self.digital_dxpsk_demod_0, 0))    
+        self.connect((self.digital_dxpsk_demod_0, 0), (self.blks2_packet_decoder_1, 0))    
+        self.connect((self.digital_dxpsk_mod_0, 0), (self.channels_channel_model_0, 0))    
+        self.connect((self.digital_map_bb_0, 0), (self.blocks_char_to_float_0, 0))    
+        self.connect((self.fec_extended_decoder_0, 0), (self.blocks_pack_k_bits_bb_0, 0))    
         self.connect((self.fec_extended_encoder_0, 0), (self.blks2_packet_encoder_1, 0))    
 
     def closeEvent(self, event):
@@ -147,6 +160,7 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
 
     def get_ccsds_encoder_var(self):
         return self.ccsds_encoder_var
